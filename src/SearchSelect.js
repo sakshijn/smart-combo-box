1562e372e4cf06116103802d21784e46064b4bad
Sakshi Jain <sakshi.myspace@gmail.com>
2022-02-22 22:31:48 -0500

    Resolved the autoFocus issue with input text

--- src/SearchSelect.js
+++ src/SearchSelect.js
@@ -7,13 +7,12 @@ import {
   faCarrot,
   faPepperHot,
 } from "@fortawesome/free-solid-svg-icons";
-import { useState, useRef } from "react";
+import { useState } from "react";
 
 export const SearchSelect = ({ data, placeholderText }) => {
   const [isOpen, setSelectOpen] = useState(false);
   const [selectedVal, setSelectedVal] = useState("");
   const [filteredItems, setFilteredItems] = useState(data);
-  const textInput = useRef(null);
 
   const filterItems = (e) => {
     const filterVal = e.currentTarget.value.toLowerCase();
@@ -42,10 +41,7 @@ export const SearchSelect = ({ data, placeholderText }) => {
         role="button"
         className={`select-btn ${isOpen ? "hide" : ""}`}
         tabIndex="-1"
-        onClick={() => {
-          textInput.current.focus();
-          setSelectOpen(true);
-        }}
+        onClick={() => setSelectOpen(true)}
       >
         <div className="selected-value">
           {selectedVal === "" ? (
@@ -56,34 +52,35 @@ export const SearchSelect = ({ data, placeholderText }) => {
         </div>
         <FontAwesomeIcon icon={faAngleDown} />
       </div>
-      <div className={`select-box-content ${isOpen ? "show focused" : ""}`}>
-        <div className="select-input">
-          <input
-            type="text"
-            autoFocus={isOpen}
-            placeholder={placeholderText}
-            className="search-box"
-            onKeyUp={filterItems}
-            ref={textInput}
-          />
-          <FontAwesomeIcon icon={faAngleDown} />
+      {isOpen && (
+        <div className={`select-box-content ${isOpen ? "focused" : ""}`}>
+          <div className="select-input">
+            <input
+              type="text"
+              autoFocus
+              placeholder={placeholderText}
+              className="search-box"
+              onKeyUp={filterItems}
+            />
+            <FontAwesomeIcon icon={faAngleDown} />
+          </div>
+          <ul className="select-options-list">
+            {filteredItems.map((item, index) => (
+              <li
+                className="select-options"
+                key={index}
+                onClick={() => {
+                  setSelectedVal(item.displayItem);
+                  setSelectOpen(false);
+                }}
+              >
+                <FontAwesomeIcon icon={getFAIcon(item.displayIcon)} />
+                <span>{item.displayItem}</span>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="select-options-list">
-          {filteredItems.map((item, index) => (
-            <li
-              className="select-options"
-              key={index}
-              onClick={() => {
-                setSelectedVal(item.displayItem);
-                setSelectOpen(false);
-              }}
-            >
-              <FontAwesomeIcon icon={getFAIcon(item.displayIcon)} />
-              <span>{item.displayItem}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      )}
     </div>
   );
 };
